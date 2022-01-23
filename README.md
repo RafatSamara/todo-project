976cd132a43585692247c5320263793fa19b0e6e
Rafat Samara <rafat.samara@hotmail.co.uk>
2022-01-24 01:22:27 +0200

    Fix Conflect

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
+<<<<<<< todo-js2
 # todo-project
 
 ## JS Task 2
+=======
+## todo-project
+
+# JS Task
+>>>>>>> main
 1. What observations or questions do you have about what you’ve learned so far?
 * Nth yet
 ---
@@ -8,6 +14,7 @@
 * 15 mnts
 ---
 3. Before you started, how long did you think it would take you to complete this assignment?
+<<<<<<< todo-js2
 * 15 mnts
 
 ---
@@ -22,3 +29,33 @@
 ---
 3. Before you started, how long did you think it would take you to complete this assignment?
 * 30 mnts
+=======
+* 30 mnts
+---
+---
+
+# CSS Task
+
+![WireFrame](./assets/wireframe2.png)
+
+1. What observations or questions do you have about what you’ve learned so far?
+- Nothing, everything is going well
+2. How long did it take you to complete this assignment? And, before you started?
+- 30 mnts
+3.  how long did you think it would take you to complete this assignment?
+- around 1 day and 30 mnts :$
+    
+---
+---
+# HTML Task
+
+![WireFrame](./assets/wireframe.png)
+
+1. What observations or questions do you have about what you’ve learned so far?
+- Nothing, everything is going well
+2. How long did it take you to complete this assignment? And, before you started?
+- 30 mnts
+3.  how long did you think it would take you to complete this assignment?
+- around 30 mnts :$ 
+    
+>>>>>>> main
--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
-// var name = prompt("Please Enter Your Name:");
+var name = prompt("Please Enter Your Name:");
 
-// var gender = prompt("Please Enter Your Gender:").toLowerCase();
+var gender = prompt("Please Enter Your Gender:").toLowerCase();
 
-// while(gender != "male" && gender != "female"){
-//     var gender = prompt("Please Enter Your Gender Again (Male/Female)").toLowerCase(); // extra step
-// }
+while(gender != "male" && gender != "female"){
+    var gender = prompt("Please Enter Your Gender Again (Male/Female)").toLowerCase(); // extra step
+}
 
-// var age = prompt("Please Enter Your Age:");
+var age = prompt("Please Enter Your Age:");
 
-// while(age <= 0 && isNaN(age) !== false ){
-//     var gender = prompt("Please Enter a correct Age");
-// }
+while(age <= 0 && isNaN(age) !== false ){
+    var gender = prompt("Please Enter a correct Age");
+}
 
-// if (confirm("Would you want to skip the welcoming message ?")){
-//     switch(gender){
-//         case "male":
-//             alert ("Welcome Mr. " + name); 
-//         break;
+if (confirm("Would you want to skip the welcoming message ?")){
+    switch(gender){
+        case "male":
+            alert ("Welcome Mr. " + name); 
+        break;
 
-//         case "female": 
-//             alert ("Welcome Ms. " + name); 
-//         break;
+        case "female": 
+            alert ("Welcome Ms. " + name); 
+        break;
 
-//         default:
-//             alert ("Welcome " + name); 
-//         break;
+        default:
+            alert ("Welcome " + name); 
+        break;
 
         
-//     }
+    }
 
-// }
+}
 
  // New Code:
  var answersArray = new Array();
